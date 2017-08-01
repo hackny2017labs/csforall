08ca52801b04e1b7c6a93c42b6fc4e5a3ef3a9ef
hemangandhi <hemangandhi@gmail.com>
2017-08-01 15:18:47 -0400

    models take 1

---
+++ cs4all/models/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Commitment(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField()
+    create_date = models.DateTimeField(auto_now = True, auto_now_add = True)
+    end_date = models.DateTimeField()
+    goal_description = models.TextField()
+    goal_target = models.IntegerField()
+    goal_current = models.IntegerField()
+    contacts = models.ManyToManyField(User)
+
+class Organization(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    create_date = models.DateTimeField(auto_now = True, auto_now_add = True)
+    contacts = models.ManyToManyField(User)
--- cs4all/settings.py
+++ cs4all/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cs4all.models'
 ]
 
 MIDDLEWARE = [
